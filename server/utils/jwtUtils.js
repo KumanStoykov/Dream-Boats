f20ebb36e3ad2276df11a05960e58b13f57c29d4
Kuman Stoykov <kumanstoykov@gmail.com>
2022-07-02 11:49:13 +0200

    Refactoring payload and added expires time

--- server/utils/jwtUtils.js
+++ server/utils/jwtUtils.js
@@ -5,11 +5,13 @@ const { SECRET } = require('../config');
 exports.createToken = (user) => {
     const payload = {
         _id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
         email: user.email,
     };
 
     return new Promise((resolve, reject) => {
-        jwt.sign(payload, SECRET, (err, token) => {
+        jwt.sign(payload, SECRET, {expiresIn: '1d'}, (err, token) => {
             if(err) {
                 reject(err);
             } else {
