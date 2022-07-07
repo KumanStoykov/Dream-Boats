50141863f5bd82a93c6ec38a0cb6057d5b67a930
Kuman Stoykov <kumanstoykov@gmail.com>
2022-07-07 16:47:13 +0200

    Added functionality

--- client/src/components/auth/Register/Register.js
+++ client/src/components/auth/Register/Register.js
@@ -1,24 +1,37 @@
+import { useNavigate, Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
-import { switchToLogin } from '../../../store/switchAuthForm';
+import { authStoreActions } from '../../../store/authStore';
+import useFetch from '../../../hooks/useFetch';
+import userRequestOptions from '../../../services/userService';
+
 
 import styles from './Register.module.css';
-
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEllipsisH } from '@fortawesome/free-solid-svg-icons';
+import Spinner from '../../ui/Spinner/Spinner';
 
 
 const Register = () => {
+    const navigate = useNavigate();
 
     const dispatch = useDispatch();
+    const { isLoading, requester } = useFetch();
 
-    const clickHandler = (e) => {
+
+    const responseData = (dataResponse) => {
+        dispatch(authStoreActions.login(dataResponse));
+    };
+
+    const submitHandler = (e) => {
         e.preventDefault();
-        
-        dispatch(switchToLogin())
-    }
+        const formData = new FormData(e.currentTarget);
+        const { firstName, lastName, email, password, repeatPassword } = Object.fromEntries(formData);
 
+        requester(userRequestOptions.register(firstName, lastName, email, password, repeatPassword), responseData);
+        navigate('/');
+    };
+    
     return (
         <section className={styles.register}>
             <div className={'container'}>
@@ -30,38 +43,73 @@ const Register = () => {
                     <div className={styles['boat-wrap']}>
                         <img src="./images/boat_register.png" alt="image.png" />
                     </div>
-                    <form className={styles['register-form']}>
+                    <form className={styles['register-form']} onSubmit={submitHandler}>
                         <div className={styles['input-group-wrap']}>
                             <div className={styles['input-group']}>
-                                <input type="text" className={styles.input} placeholder="First name" />
+                                <input
+                                    type="text"
+                                    className={styles.input}
+                                    placeholder="First name"
+                                    name='firstName'
+                                    disabled={isLoading}
+                                />
                                 <span className={styles.bar}></span>
                             </div>
                             <div className={styles['input-group']}>
-                                <input type="text" className={styles.input} placeholder="Last name" />
+                                <input
+                                    type="text"
+                                    className={styles.input}
+                                    placeholder="Last name"
+                                    name='lastName'
+                                    disabled={isLoading}
+                                />
                                 <span className={styles.bar}></span>
                             </div>
                         </div>
 
                         <div className={styles['input-group']}>
-                            <input type="email" className={styles.input} placeholder="Email" />
+                            <input
+                                type="email"
+                                className={styles.input}
+                                placeholder="Email"
+                                name='email'
+                                disabled={isLoading}
+                            />
                             <span className={styles.bar}></span>
                         </div>
 
                         <div className={styles['input-group']}>
-                            <input type="password" className={styles.input} placeholder="Password" />
+                            <input
+                                type="password"
+                                className={styles.input}
+                                placeholder="Password"
+                                name='password'
+                                disabled={isLoading}
+                            />
                             <span className={styles.bar}></span>
                         </div>
 
                         <div className={styles['input-group']}>
-                            <input type="password" className={styles.input} placeholder="Re-password" />
+                            <input
+                                type="password"
+                                className={styles.input}
+                                placeholder="Re-password"
+                                name='repeatPassword'
+                                disabled={isLoading}
+                            />
                             <span className={styles.bar}></span>
                         </div>
 
                         <div className={styles['input-group']}>
-                            <p className={styles['register-now']}>Already have account? <a href="/auth/login" onClick={clickHandler}>Login Now!</a></p>
+                            <p className={styles['register-now']}>Already have account? <Link to="/auth/login" >Login Now!</Link></p>
                         </div>
 
-                        <button type="submit" className={'btn-blue'}>Register
+                        <button
+                            type="submit"
+                            className={'btn-blue'}
+                            disabled={isLoading}
+                        >Register
+                            
                             <span className={'dots'}><FontAwesomeIcon icon={faEllipsisH} /></span>
                         </button>
                     </form>
