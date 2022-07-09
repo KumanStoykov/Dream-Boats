dab7c64efce5609296dd4c1374dcd079d0743bc4
Kuman Stoykov <kumanstoykov@gmail.com>
2022-07-09 11:37:56 +0200

    Create checkCredentialMiddleware

---
+++ server/middleware/checkCredentialMiddleware.js
@@ -0,0 +1,17 @@
+const { jwtVerify } = require('../utils/jwtUtils');
+
+const { COOKIE_TOKEN_NAME, SECRET } = require('../config');
+
+module.exports = (req, res, next) => {
+    const token = req.cookies[COOKIE_TOKEN_NAME];
+
+    try{
+        const decodedToken = jwtVerify(token, SECRET);
+        
+        req.decodedToken = decodedToken;
+        next();
+    } catch(error) {
+        res.decodedToken = undefined;
+        next();
+    }
+}
