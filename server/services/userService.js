649e1a3760aa528d501b228423e98f93e8937974
Kuman Stoykov <kumanstoykov@gmail.com>
2022-07-09 11:27:58 +0200

    Update user service

--- server/services/userService.js
+++ server/services/userService.js
@@ -2,6 +2,7 @@ const User = require('../models/User');
 
 
 exports.getByEmail = (email) => User.findOne({ email });
+
 exports.getById = (id) => User.findById(id);
 
-exports.createUser = (firstName, lastName, email, password) => User.create(firstName, lastName, email, password); 
+exports.createUser = (firstName, lastName, email, password) => User.create({ firstName, lastName, email, password }); 
