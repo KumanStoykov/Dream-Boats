c6369d85db26f9069e7730cb0e1de0c02086288a
Kuman Stoykov <kumanstoykov@gmail.com>
2022-07-09 11:37:30 +0200

    Added cloudinary config

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const router = express.Router();
+
+const cloudinary = require('cloudinary').v2;
 
 const config = require('./config');
 const expressInit = require('./config/express');
@@ -9,10 +12,22 @@ const app = express();
 expressInit(app);
 
 
+cloudinary.config(config.CLOUDINARY);
+
 
 databaseInit(config.DB_CONNECTION_STRING)
     .then(() => {
-        app.listen(config.PORT, console.log.bind(`App listen in port: ${config.PORT}...`));
+        router.get('*', (req, res) => {
+
+            const formats = ['.js', '.css', '.ico', '.jpg', '.png']
+
+            if (formats.filter(ext => req.url.indexOf(ext) > 0).length > 0) {
+                res.sendFile(path.resolve(`public/${req.url}`));
+            } else {
+                res.sendFile(path.join(__dirname, "public/index.html"));
+            }
+        });
+        app.listen(config.PORT, () => console.log(`App listen in port: ${config.PORT}...`));
     })
     .catch(err => console.log('Database init is failed!', err));
 
