94547befd7aad2bc80cd280257014a1419490738
Kuman Stoykov <kumanstoykov@gmail.com>
2022-07-09 19:02:17 +0200

    Added Spiner

--- client/src/components/auth/Login/Login.js
+++ client/src/components/auth/Login/Login.js
@@ -1,18 +1,18 @@
 import { useNavigate, Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
-import { authStoreActions } from '../../../store/authStore';
-import useFetch from '../../../hooks/useFetch';
-import userRequestOptions from '../../../services/userService';
-
-import styles from './Login.module.css';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEllipsisH } from '@fortawesome/free-solid-svg-icons';
 
+import { authStoreActions } from '../../../store/authStore';
+import useFetch from '../../../hooks/useFetch';
+import userService from '../../../services/userService';
+import Spinner from '../../ui/Spinner/Spinner';
+
+import styles from './Login.module.css';
 
 
-const Login = () => {  
+const Login = () => {
     const navigate = useNavigate();
 
     const dispatch = useDispatch();
@@ -22,18 +22,18 @@ const Login = () => {
         dispatch(authStoreActions.login(dataResponse));
     };
 
-    const submitHandler = (e) => {
+    const submitHandler = async (e) => {
         e.preventDefault();
 
         const formData = new FormData(e.currentTarget);
-        const {email, password} = Object.fromEntries(formData);
+        const { email, password } = Object.fromEntries(formData);
 
-        requester(userRequestOptions.login(email,password), responseData);
+        requester(userService.login(email, password), responseData);
         navigate('/');
-    }   
+    }
 
 
- 
+
     return (
         <section className={styles.login}>
             <div className={'container'}>
@@ -43,28 +43,28 @@ const Login = () => {
                 </h5>
                 <div className={styles['login-content']}>
                     <div className={styles['boat-wrap']}>
-                        <img src="./images/boat_login.png" alt="image.png" />
+                        <img src="/images/boat_login.png" alt="image.png" />
                     </div>
                     <form className={styles['login-form']} onSubmit={submitHandler}>
 
                         <div className={styles['input-group']}>
-                            <input 
-                            type="email" 
-                            className={styles.input} 
-                            placeholder="Email" 
-                            name={'email'}
-                            disabled={isLoading}
+                            <input
+                                type="email"
+                                className={styles.input}
+                                placeholder="Email"
+                                name={'email'}
+                                disabled={isLoading}
                             />
                             <span className={styles.bar}></span>
                         </div>
 
                         <div className={styles['input-group']}>
-                            <input 
-                            type="password" 
-                            className={styles.input}
-                            placeholder="Password"
-                            name={'password'}
-                            disabled={isLoading} 
+                            <input
+                                type="password"
+                                className={styles.input}
+                                placeholder="Password"
+                                name={'password'}
+                                disabled={isLoading}
                             />
                             <span className={styles.bar}></span>
                         </div>
@@ -73,12 +73,16 @@ const Login = () => {
                             <p className={styles['register-now']}>Not registered yet? <Link to="/auth/register" >Register Now!</Link></p>
                         </div>
 
-                        <button 
-                            type="submit" 
+                        <button
+                            type="submit"
                             className={'btn-blue'}
                             disabled={isLoading}
                         >Login
-                            <span className={'dots'}><FontAwesomeIcon icon={faEllipsisH} /></span>
+                            {isLoading
+                                ? <Spinner size={'small'} />
+                                : <span className={'dots'}><FontAwesomeIcon icon={faEllipsisH} /></span>
+                            }
+
                         </button>
                     </form>
                 </div>
