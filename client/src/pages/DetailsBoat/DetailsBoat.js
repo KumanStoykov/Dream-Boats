f3bd745cdaa1379647354f1ea398956be0371c34
Kuman Stoykov <kumanstoykov@gmail.com>
2022-07-10 23:16:34 +0200

    Create DetailsBoat

---
+++ client/src/pages/DetailsBoat/DetailsBoat.js
@@ -0,0 +1,13 @@
+import DetailsCard from '../../components/details/DetailsCard/DetailsCard';
+
+
+const DetailsBoat = () => {
+
+    return (
+        <>
+        <DetailsCard />
+        </>
+    );
+};
+
+export default DetailsBoat;
