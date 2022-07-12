28c161288e2a7d74c4cb4419ee304f9aa4225efd
Kuman Stoykov <kumanstoykov@gmail.com>
2022-07-12 23:52:58 +0200

    Change directory

---
+++ client/src/components/boat/boatsCatalog/BoatsList/BoatsList.js
@@ -0,0 +1,50 @@
+import { useCallback, useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDharmachakra } from '@fortawesome/free-solid-svg-icons';
+
+import useFetch from '../../../../hooks/useFetch';
+import boatService from '../../../../services/boatService';
+
+import BoatCard from '../BoatCard/BoatCard';
+import Spinner from '../../../ui/Spinner/Spinner';
+
+
+import styles from './BoatsList.module.css';
+
+
+
+
+const BoatsList = () => {
+    const dispatch = useDispatch();
+    const { isLoading, requester } = useFetch();
+    const [boats, setBoats] = useState([]);
+
+    const responseData = useCallback((data) => {
+        setBoats(data.allBoats);
+    }, []);
+
+    useEffect(() => {
+        requester(boatService.getAll(), responseData)
+    }, []);
+ 
+
+    return (
+        <section className={styles.boats}>
+            <div className={'container'}>
+                <h5 className={styles['section-head']}>
+                    <span className={styles.heading}>Boats</span>
+                    <span className={styles['anchor-heading']}><FontAwesomeIcon icon={faDharmachakra} /></span>
+                </h5>
+                <ul className={'grid'}>
+                  {boats.map(x => <BoatCard key={x._id} boat={x}/>)}
+                </ul>
+            </div>
+        </section>
+
+    );
+
+};
+
+export default BoatsList;
