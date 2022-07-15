b4fb58c61025a035147f52e5fd30bb8c73654c13
Kuman Stoykov <kumanstoykov@gmail.com>
2022-07-15 17:00:02 +0200

    Change token name

--- server/config/index.js
+++ server/config/index.js
@@ -1,13 +1,13 @@
 const path = require('path');
 
-
 require('dotenv').config({ path: path.resolve(__dirname, '../.env')});
+
 
 const configEnv = {
     development: {
         PORT: process.env.PORT || 5000,
         DB_CONNECTION_STRING: 'mongodb://localhost:27017/dreamBoats',
-        TOKEN_NAME: 'X-Authorization',
+        COOKIE_TOKEN_NAME: 'X-Authorization',
         SECRET: '07505766beb4ea7ea441c0f0481799cee1c08b96c38b4f2ba427671ce5a6fa25fd9246fc841ae8b088bb35cb4d3d182d6ce8edf61ffc73daf05f07931b919e35',
         ROUND_SALT: 9,
         CORS: {
@@ -17,13 +17,13 @@ const configEnv = {
         CLOUDINARY: {
             api_key: process.env.CLOUDINARY_API_KEY,
             api_secret: process.env.CLOUDINARY_API_SECRET,
-            cloud_name: process.env.CLOUDINARY_CLOUD_NAME
+            cloud_name: process.env.CLOUDINARY_CLOUD_NAME 
         }
     },
     production: {
         PORT: process.env.PORT || 80,
         DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING,
-        TOKEN_NAME: process.env.TOKEN_NAME,
+        COOKIE_TOKEN_NAME: process.env.COOKIE_TOKEN_NAME,
         SECRET: process.env.SECRET,
         ROUND_SALT: process.env.ROUND_SALT,
         CORS: {
