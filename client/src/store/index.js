f2e1aaef67b07dc6639266d939a2206a778e5a92
Kuman Stoykov <kumanstoykov@gmail.com>
2022-07-15 17:03:16 +0200

    Added modalStore

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -13,6 +13,7 @@ import {
 
 import authStore from './authStore';
 import boatStore from './boatStore';
+import modalStore from './modalStore';
 
 
 const persistConfig = {
@@ -24,6 +25,7 @@ const persistConfig = {
 const rootReducer = combineReducers({
     auth: authStore,
     boat: boatStore,
+    modal: modalStore,
 });
 
 
