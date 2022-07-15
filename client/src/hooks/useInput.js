3b1efa6665dfa912860a5f835290b8e968697e28
Kuman Stoykov <kumanstoykov@gmail.com>
2022-07-15 17:09:58 +0200

    Create useInput

---
+++ client/src/hooks/useInput.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+const useInput = (validation) => {
+    const [value, setValue] = useState('');
+    const [hasTouched, setHasTouched] = useState(false);
+
+    const onChange = (e) => {
+        setValue(e.target.value);        
+    }
+    const onBlur = () => {
+        setHasTouched(true);
+    };
+
+    const reset = () => {
+        setValue('');
+        setHasTouched(false);
+    }
+
+    const fieldIsValid = validation(value);
+    const hasError = !fieldIsValid && hasTouched;
+
+    return{
+        value,
+        fieldIsValid,
+        hasError,
+        onChange,
+        onBlur,
+        reset
+    }
+}
+
+export default useInput;
