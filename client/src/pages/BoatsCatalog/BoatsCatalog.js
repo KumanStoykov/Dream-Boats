d1b6b07859d3cd594aeddfdb21f6725b5313b586
Kuman Stoykov <kumanstoykov@gmail.com>
2022-07-21 20:57:44 +0200

    Added fetch requester

--- client/src/pages/BoatsCatalog/BoatsCatalog.js
+++ client/src/pages/BoatsCatalog/BoatsCatalog.js
@@ -1,18 +1,42 @@
+import { useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-import BoatsList from '../../components/boat/boatsCatalog/BoatsList/BoatsList';
+import useFetch from '../../hooks/useFetch';
+import boatService from '../../services/boatService';
+import { boatStoreActions } from '../../store/boatStore';
+
+import BoatsList from '../../components/boat/BoatList/BoatList';
+import BoatCatalogHero from '../../components/boat/BoatCatalogHero/BoatCatalogHero';
+
 import Search from '../../components/ui/Search/Search';
-import BoatsCatalogHero from '../../components/boat/boatsCatalog/BoatsCatalogHero/BoatsCatalogHero';
+import Spinner from '../../components/ui/Spinner/Spinner';
 
 
+import styles from '../../components/boat/BoatList/BoatList.module.css';
 
 const BoatsCatalog = () => {
-  
+    const dispatch = useDispatch();
+    const boats = useSelector(state => state.allBoats.boats);
+    const { isLoading, requester } = useFetch();
+
+
+    const responseData = useCallback((data) => {
+        dispatch(boatStoreActions.addBoats(data));
+    }, [dispatch]);
+
+    
+    useEffect(() => {
+        requester(boatService.getAll(), responseData);        
+    }, [requester, responseData]);
 
     return (
         <>
-            <BoatsCatalogHero />
+            <BoatCatalogHero />
             <Search />
-            <BoatsList />         
+            {isLoading && <Spinner size={'large'} />}
+            <section className={styles.boats}>
+                <BoatsList boats={boats}/>
+            </section>
         </>
     )
 };
