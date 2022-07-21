230084043bad663c89d146134f0175f5b4ecf57c
Kuman Stoykov <kumanstoykov@gmail.com>
2022-07-21 21:03:52 +0200

    Change config data

--- server/config/index.js
+++ server/config/index.js
@@ -8,7 +8,7 @@ const configEnv = {
         PORT: process.env.PORT || 5000,
         DB_CONNECTION_STRING: 'mongodb://localhost:27017/dreamBoats',
         COOKIE_TOKEN_NAME: 'X-Authorization',
-        SECRET: '07505766beb4ea7ea441c0f0481799cee1c08b96c38b4f2ba427671ce5a6fa25fd9246fc841ae8b088bb35cb4d3d182d6ce8edf61ffc73daf05f07931b919e35',
+        SECRET: process.env.SECRET,
         ROUND_SALT: 9,
         CORS: {
             origin: ['http://localhost:3000'],
