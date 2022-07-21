45334a61fe0567e0263cca6f526b3c25c3ce5fde
Kuman Stoykov <kumanstoykov@gmail.com>
2022-07-21 21:03:15 +0200

    Added current format validations

--- client/src/validation/boatValidation.js
+++ client/src/validation/boatValidation.js
@@ -1,18 +1,21 @@
 import validator from 'validator';
 
-const isLength = (value) => validator.isLength(value, { min: 3 });
+const isLengthThreeCh = (value) => validator.isLength(value, { min: 3 });
+
+const isLengthTwoCh = (value) => validator.isLength(value, { min: 2 });
 
 const descriptionLength = (value) => validator.isLength(value, { min: 20 });
 
-const numberCheck = (value) => validator.isInt(value);
+const checkYear = (value) => validator.isInt(value, { min: 1960, max: 2022 });
 
-const stringFormat = (value) => validator.matches(value, /^[a-zA-Z,]+$/);
+const isEmpty = (value) => !(validator.isEmpty(value));
 
 const boatValidation = {
-    isLength,
+    isLengthThreeCh,
+    isLengthTwoCh,
     descriptionLength,
-    numberCheck,
-    stringFormat
+    checkYear,
+    isEmpty
 };
 
 export default boatValidation;
