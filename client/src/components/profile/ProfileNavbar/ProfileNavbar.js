7b2b78cbc0623731215d62d4adaa7e7a6d39a94b
Kuman Stoykov <kumanstoykov@gmail.com>
2022-07-21 20:18:38 +0200

    Added links

--- client/src/components/profile/ProfileNavbar/ProfileNavbar.js
+++ client/src/components/profile/ProfileNavbar/ProfileNavbar.js
@@ -1,9 +1,8 @@
 import { Link } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserEdit, faSailboat } from '@fortawesome/free-solid-svg-icons';
+import { faUserEdit, faSailboat, faMoneyCheckDollar } from '@fortawesome/free-solid-svg-icons';
 import { faEye } from '@fortawesome/free-regular-svg-icons';
-
 
 import styles from './ProfileNavbar.module.css';
 
@@ -12,24 +11,28 @@ const ProfileNavbar = () => {
 
 
     return (
-
-        <div className={`${styles['nav-container']} ${styles['nav-wrapper']}`}>
-            <nav className={styles['nav-horizontals']}>
-                <ul className={styles['nav-horizontals-list']}>
-                    <li className={styles['nav-horizontals-item']}>
-                        <Link to="/boats-catalog" className={styles['nav-horizontals-link']}><FontAwesomeIcon className={styles['user-profile-icon']} icon={faUserEdit} /></Link>
+        <div className={`${styles['nav-container']}`}>
+            <nav className={styles['nav-horizontals']}>           
+                <ul className={styles['container-profile-nav']}>
+                    <li className={styles.box}>
+                    <Link to="/boats-catalog" className={styles['nav-horizontals-link']}><FontAwesomeIcon className={styles['user-profile-icon']} icon={faUserEdit} /></Link>
                     </li>
-                    <li className={styles['nav-horizontals-item']}>
-                        <Link to="/details" className={styles['nav-horizontals-link']}><FontAwesomeIcon className={styles['user-profile-icon']} icon={faSailboat} /></Link>
+                    <li className={styles.box}>
+                    <Link to="/boats-catalog" className={styles['nav-horizontals-link']}><FontAwesomeIcon className={styles['user-profile-icon']} icon={faSailboat} /></Link>
                     </li>
-                    <li className={styles['nav-horizontals-item']}>
-                        <Link to="/news" className={styles['nav-horizontals-link']}><FontAwesomeIcon className={styles['user-profile-icon']} icon={faEye} /></Link>
-                    </li>                  
+                    <li className={styles.box}>
+                    <Link to="/boats-catalog" className={styles['nav-horizontals-link']}><FontAwesomeIcon className={styles['user-profile-icon']} icon={faMoneyCheckDollar} /></Link>
+                    </li>                   
+                    <li className={styles.box}>
+                    <Link to="/boats-catalog" className={styles['nav-horizontals-link']}><FontAwesomeIcon className={styles['user-profile-icon']} icon={faEye} /></Link>
+                    </li>
                 </ul>
+
             </nav>
         </div>
-
     );
 };
 
-export default ProfileNavbar
+export default ProfileNavbar;
+
+
