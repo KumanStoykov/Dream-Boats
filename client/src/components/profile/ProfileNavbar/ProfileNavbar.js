0d0115832d26b0c4def076a9e83408cca5337feb
Kuman Stoykov <kumanstoykov@gmail.com>
2022-07-25 22:08:59 +0200

    Change some styles

--- client/src/components/profile/ProfileNavbar/ProfileNavbar.js
+++ client/src/components/profile/ProfileNavbar/ProfileNavbar.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserEdit, faSailboat, faMoneyCheckDollar } from '@fortawesome/free-solid-svg-icons';
+import { faUserEdit, faSailboat, faEnvelope, faCheck } from '@fortawesome/free-solid-svg-icons';
 import { faEye } from '@fortawesome/free-regular-svg-icons';
 
 import styles from './ProfileNavbar.module.css';
@@ -12,22 +12,32 @@ const ProfileNavbar = () => {
 
     return (
         <div className={`${styles['nav-container']}`}>
-            <nav className={styles['nav-horizontals']}>           
-                <ul className={styles['container-profile-nav']}>
-                    <li className={styles.box}>
-                    <Link to="/boats-catalog" className={styles['nav-horizontals-link']}><FontAwesomeIcon className={styles['user-profile-icon']} icon={faUserEdit} /></Link>
-                    </li>
-                    <li className={styles.box}>
-                    <Link to="/boats-catalog" className={styles['nav-horizontals-link']}><FontAwesomeIcon className={styles['user-profile-icon']} icon={faSailboat} /></Link>
-                    </li>
-                    <li className={styles.box}>
-                    <Link to="/boats-catalog" className={styles['nav-horizontals-link']}><FontAwesomeIcon className={styles['user-profile-icon']} icon={faMoneyCheckDollar} /></Link>
-                    </li>                   
-                    <li className={styles.box}>
-                    <Link to="/boats-catalog" className={styles['nav-horizontals-link']}><FontAwesomeIcon className={styles['user-profile-icon']} icon={faEye} /></Link>
-                    </li>
-                </ul>
-
+            <nav className={styles['user-nav']}>           
+                    <ul className={styles['container-profile-nav']}>
+                        <li className={styles.box}>
+                            <Link to="edit-profile" className={styles['user-nav-link']}>
+                                <FontAwesomeIcon className={styles['user-profile-icon']} icon={faUserEdit} />
+                            </Link>
+                        </li>
+                        <li className={styles.box}>
+                            <Link to="sell-boat" className={styles['user-nav-link']}>
+                                <FontAwesomeIcon className={styles['user-profile-icon']} icon={faSailboat} />
+                            </Link>
+                        </li>
+                        <li className={styles.box}>
+                            <Link to="message" className={styles['user-nav-link']}>
+                                <FontAwesomeIcon className={`${styles['user-profile-icon']}`} icon={faEnvelope} />
+                                <span className={styles['number-message']}>
+                                    <FontAwesomeIcon icon={faCheck}/>
+                                </span>
+                            </Link>
+                        </li>                   
+                        <li className={styles.box}>
+                            <Link to="watched" className={styles['user-nav-link']}>
+                                <FontAwesomeIcon className={styles['user-profile-icon']} icon={faEye} />
+                            </Link>
+                        </li>
+                    </ul>
             </nav>
         </div>
     );
