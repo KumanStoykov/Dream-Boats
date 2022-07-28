db7f503a98cc6b8272db981e618e6f1f96869c84
Kuman Stoykov <kumanstoykov@gmail.com>
2022-07-28 19:29:14 +0200

    Created MyBoats

---
+++ client/src/components/profile/MyBoats/MyBoats.js
@@ -0,0 +1,51 @@
+import { useCallback, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+import useFetch from '../../../hooks/useFetch';
+import boatService from '../../../services/boatService';
+import { boatStoreActions } from '../../../store/boatStore';
+
+
+import BoatsList from '../../boat/BoatList/BoatList';
+import NoBoatsHeading from '../../ui/NoBoatsHeading/NoBoatsHeading';
+import Spinner from '../../ui/Spinner/Spinner';
+
+const MyBoats = () => {
+    const { search } = useLocation();
+
+    const dispatch = useDispatch();
+    const boats = useSelector(state => state.allBoats.boats);
+    const { isLoading, requester } = useFetch();
+
+    const heading = 'Boats';
+    const subHeading = 'Your Offers';
+
+    const responseData = useCallback((data) => {
+        dispatch(boatStoreActions.addBoats(data));
+    }, [dispatch]);
+
+    useEffect(() => {
+        requester(boatService.getByOwner(search), responseData);
+    }, [requester, responseData, dispatch]);
+
+
+    return (
+        <>
+            {isLoading && <Spinner size={'large'} />}
+            {!isLoading && boats.length > 0
+                && <BoatsList
+                    boats={boats}
+                    heading={heading}
+                    subHeading={subHeading}
+                />
+            }
+            {!isLoading && boats.length < 1
+                && <NoBoatsHeading />
+            }
+        </>
+    );
+
+};
+
+export default MyBoats;
