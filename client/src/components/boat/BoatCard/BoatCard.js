f1fafd9fdf9f5fdc74cbcc03f96c08714b8a3871
Kuman Stoykov <kumanstoykov@gmail.com>
2022-07-29 15:11:07 +0200

    Added link and formatted price

--- client/src/components/boat/BoatCard/BoatCard.js
+++ client/src/components/boat/BoatCard/BoatCard.js
@@ -13,7 +13,7 @@ const BoatCard = ({
         <li className={`grid-item ${styles['featured-boats']}`}>
             <img src={boat.image[0]?.url} alt="image.jpg" className={`${styles['boat-image']} ${styles[fancyBoarder]}`} />
             <h5 className={styles['boat-name']}>{boat.model}</h5>
-            <span className={styles['boat-price']}>{boat.price}$</span>
+            <span className={styles['boat-price']}>{boat.price.toLocaleString()}$</span>
             <Link to={`/boat/details/${boat._id}`} className={`btn-gradient  ${styles['btn-down']}`}>Show offer
                 <span className={'dots'}><FontAwesomeIcon icon={faEllipsisH} /></span>
             </Link>
--- client/src/components/details/DetailsCard/DetailsCard.js
+++ client/src/components/details/DetailsCard/DetailsCard.js
@@ -1,5 +1,7 @@
+import { Link } from 'react-router-dom';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEllipsisH, faEnvelope, faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faXmarkSquare, faPenSquare, faEnvelope, faCheck } from '@fortawesome/free-solid-svg-icons';
 import { faEye } from '@fortawesome/free-regular-svg-icons';
 
 import Slideshow from '../../ui/Slider/Slider';
@@ -8,6 +10,7 @@ import styles from './DetailsCard.module.css';
 
 
 const DetailsCard = ({ boat }) => {
+
 
 
     return (
@@ -24,7 +27,7 @@ const DetailsCard = ({ boat }) => {
                 <a href="/" className={styles['product-link']}><FontAwesomeIcon icon={faEye} /></a>
 
                 <div className={styles['product-price']}>
-                    <p className={styles['new-price']}>Price: <span>${boat.price}</span></p>
+                    <p className={styles['new-price']}>Price: <span>${boat.price.toLocaleString()}</span></p>
                 </div>
                 <div className={styles['product-detail']}>
                     <h2>About This Boat: </h2>
@@ -34,20 +37,22 @@ const DetailsCard = ({ boat }) => {
                         <li><FontAwesomeIcon className={styles.check} icon={faCheck} /> Year: <span>{boat.year}</span></li>
                         <li><FontAwesomeIcon className={styles.check} icon={faCheck} /> Condition: <span>{boat.condition}</span></li>
                         <li><FontAwesomeIcon className={styles.check} icon={faCheck} /> Type: <span>{boat.type}</span></li>
-                        <li><FontAwesomeIcon className={styles.check} icon={faCheck} /> Length: <span>{boat.length} m.</span></li>
-                        <li><FontAwesomeIcon className={styles.check} icon={faCheck} /> Category: <span>{boat.category}</span></li>
+                        <li><FontAwesomeIcon className={styles.check} icon={faCheck} /> Length: <span>{boat.boatLength} m.</span></li>
                         <li><FontAwesomeIcon className={styles.check} icon={faCheck} /> Engine Make: <span>{boat.engineMake}</span></li>
                         <li><FontAwesomeIcon className={styles.check} icon={faCheck} /> Fuel type: <span>{boat.fuel}</span></li>
                         <li><FontAwesomeIcon className={styles.check} icon={faCheck} /> Hull Material: <span>{boat.hullMaterial}</span></li>
                         <li><FontAwesomeIcon className={styles.check} icon={faCheck} /> Location: <span>{boat.location}</span></li>
                     </ul>
                 </div>
-                <div className={styles['purchase-info']}>
+                <div className={styles['btn-group']}>
+                    <Link to={`/profile/${boat._id}/edit`} className={'btn-gradient'}>
+                        Edit <FontAwesomeIcon className={styles.icons} icon={faPenSquare} />
+                    </Link>
                     <button type="submit" className={'btn-gradient'}>
-                        Bay <FontAwesomeIcon className={'dots'} icon={faEllipsisH} />
+                        Delete <FontAwesomeIcon className={styles.icons} icon={faXmarkSquare} />
                     </button>
                     <button type="submit" className={'btn-gradient'}>
-                        Contact <FontAwesomeIcon className={styles.message} icon={faEnvelope} />
+                        Contact <FontAwesomeIcon className={styles.icons} icon={faEnvelope} />
                     </button>
                 </div>
             </div>
