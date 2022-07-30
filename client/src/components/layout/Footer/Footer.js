16ea8acc1578376d0782e26d4e2a890b1a479b13
Kuman Stoykov <kumanstoykov@gmail.com>
2022-07-30 10:43:00 +0200

    Added  url paths

--- client/src/components/layout/Footer/Footer.js
+++ client/src/components/layout/Footer/Footer.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter, faFacebookSquare, faInstagram, faPinterest, faGoogle } from '@fortawesome/free-brands-svg-icons';
 
@@ -15,30 +17,30 @@ const Footer = () => {
             <div className={'container'}>
                 <div className={styles['footer-content']}>
                     <div className={styles['footer-content-about']}>
-                        <a href="index.html" className={styles.logo}>
-                            <img className={styles['logo-image']} src="/images/logo.png" alt="logo.png" />
-                        </a>
+                        <Link to='/' className={styles.logo}>
+                            <img className={styles['logo-image']} src='/images/logo.png' alt='logo.png' />
+                        </Link>
                         <ul className={styles['about-section']} >
-                            <li><a className={styles['about-section-item']} href="#">About us</a></li>
-                            <li><a className={styles['about-section-item']} href="#">Contact</a></li>
-                            <li><a className={styles['about-section-item']}  href="#">Terms and Conditions</a></li>
-                        </ul>                       
+                            <li><Link className={styles['about-section-item']} to='/about'>About us</Link></li>
+                            <li><Link className={styles['about-section-item']} to='/contact'>Contact</Link></li>
+                            <li><Link className={styles['about-section-item']} to='/terms-and-condition'>Terms and Conditions</Link></li>
+                        </ul>
                     </div>
 
                     <div className={styles['social-media-wrap']}>
                         <h4 className={styles['footer-heading']}>Follow us</h4>
                         <div className={styles['social-media']}>
-                            <a href="#" className={styles['sm-link']}><FontAwesomeIcon icon={faTwitter} /></a>
-                            <a href="#" className={styles['sm-link']}><FontAwesomeIcon icon={faFacebookSquare} /></a>
-                            <a href="#" className={styles['sm-link']}><FontAwesomeIcon icon={faInstagram} /></a>
-                            <a href="#" className={styles['sm-link']}><FontAwesomeIcon icon={faPinterest} /></a>
-                            <a href="#" className={styles['sm-link']}><FontAwesomeIcon icon={faGoogle} /></a>
+                            <a target='_blank' href='https://www.facebook.com' className={styles['sm-link']}><FontAwesomeIcon icon={faFacebookSquare} /></a>
+                            <a target='_blank' href='https://twitter.com' className={styles['sm-link']}><FontAwesomeIcon icon={faTwitter} /></a>
+                            <a target='_blank' href='https://www.instagram.com' className={styles['sm-link']}><FontAwesomeIcon icon={faInstagram} /></a>
+                            <a target='_blank' href='https://www.pinterest.com' className={styles['sm-link']}><FontAwesomeIcon icon={faPinterest} /></a>
+                            <a target='_blank' href='https://www.google.com' className={styles['sm-link']}><FontAwesomeIcon icon={faGoogle} /></a>
                         </div>
                     </div>
                 </div>
             </div>
             <div className={styles['p-container']}>
-            <p className={styles['end-paragraph']}>&copy;Education purpose only! No rights reserved!</p>
+                <p className={styles['end-paragraph']}>&copy;Education purpose only! No rights reserved!</p>
 
             </div>
         </footer>
