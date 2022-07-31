3dce62d469e689b11061d1a9c0132342c365d98d
Kuman Stoykov <kumanstoykov@gmail.com>
2022-07-31 20:23:31 +0200

    Added boat make

--- client/src/components/boat/BoatCard/BoatCard.js
+++ client/src/components/boat/BoatCard/BoatCard.js
@@ -12,7 +12,8 @@ const BoatCard = ({
     return (
         <li className={`grid-item ${styles['featured-boats']}`}>
             <img src={boat.image[0]?.url} alt="image.jpg" className={`${styles['boat-image']} ${styles[fancyBoarder]}`} />
-            <h5 className={styles['boat-name']}>{boat.model}</h5>
+            <h5 className={styles['boat-make']}>{boat.make}</h5>
+            <p className={styles['boat-model']}>{boat.model}</p>
             <span className={styles['boat-price']}>{boat.price.toLocaleString()}$</span>
             <Link to={`/boat/details/${boat._id}`} className={`btn-gradient  ${styles['btn-down']}`}>Show offer
                 <span className={'dots'}><FontAwesomeIcon icon={faEllipsisH} /></span>
