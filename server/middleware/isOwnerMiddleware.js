52dc250a87641500e87b943c15681e36914b1fe6
Kuman Stoykov <kumanstoykov@gmail.com>
2022-08-01 11:18:05 +0200

    Refactoring owner middleware

--- server/middleware/isOwnerMiddleware.js
+++ server/middleware/isOwnerMiddleware.js
@@ -1,13 +1,18 @@
 const boatService = require('../services/boatService');
+const { jwtVerify } = require('../utils/jwtUtils');
+
+const { COOKIE_TOKEN_NAME, SECRET } = require('../config');
 
 
-exports.isOwner = () => async (req, res, next) => {
+module.exports = () => async (req, res, next) => {
+    const token = req.cookies[COOKIE_TOKEN_NAME];
+
     try {
-        const userId = req.user._id;
+        const user = await jwtVerify(token, SECRET);
 
-        const boat = await boatService.getOne(req.params.id);
-
-        const isOwn = userId == boat.owner._id;
+        const boat = await boatService.getOne(req.params.boatId);
+        
+        const isOwn = user._id == boat.owner._id;
 
         if (isOwn) {
             next();
