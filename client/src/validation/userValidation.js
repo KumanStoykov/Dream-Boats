92c859ea8b6d3ec76cd8ea4d8cbf88441ef2f7e0
Kuman Stoykov <kumanstoykov@gmail.com>
2022-08-01 11:17:40 +0200

    Change validations value

--- client/src/validation/userValidation.js
+++ client/src/validation/userValidation.js
@@ -2,14 +2,17 @@ import validator from 'validator';
 
 const emailIsValid = (value) => validator.isEmail(value);
 
+const isPhone = (value) => validator.isMobilePhone(value);
+
 const passwordIsLength = (value) => validator.isLength(value, { min: 5 });
 
-const nameIsLength = (value) => validator.isLength(value, { min: 4 });
+const nameIsLength = (value) => validator.isLength(value, { min: 2 });
 
 const isEqual = (pass, rePass) => validator.equals(pass, rePass);
 
 const userValidation = {
     emailIsValid,
+    isPhone,
     passwordIsLength,
     nameIsLength,
     isEqual
