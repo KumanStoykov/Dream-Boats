5ed8de871122d0ec4bfb8cfe4923da5d8658bece
Kuman Stoykov <kumanstoykov@gmail.com>
2022-08-01 11:19:16 +0200

    Added createUser and editUser

--- server/services/userService.js
+++ server/services/userService.js
@@ -5,4 +5,11 @@ exports.getByEmail = (email) => User.findOne({ email });
 
 exports.getById = (id) => User.findById(id);
 
-exports.createUser = (firstName, lastName, email, password) => User.create({ firstName, lastName, email, password }); 
+exports.createUser = (firstName, lastName, email, phone, password) => User.create({ firstName, lastName, email, phone, password });
+
+exports.editUser = async (firstName, lastName, email, phone, userId) => {
+   const user = await User.findById(userId);
+
+   Object.assign(user, { firstName, lastName, email, phone });
+    return user.save();
+}; 
