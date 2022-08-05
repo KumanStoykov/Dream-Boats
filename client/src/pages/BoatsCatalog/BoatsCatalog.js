ba55a50735590ac9ee6c2b56634201b9f6445a59
Kuman Stoykov <kumanstoykov@gmail.com>
2022-08-05 23:41:30 +0200

    Added pagination and no boats heading

--- client/src/pages/BoatsCatalog/BoatsCatalog.js
+++ client/src/pages/BoatsCatalog/BoatsCatalog.js
@@ -1,4 +1,5 @@
 import { useCallback, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -10,36 +11,44 @@ import { boatStoreActions } from '../../store/boatStore';
 
 import BoatsList from '../../components/boat/BoatList/BoatList';
 import BoatCatalogHero from '../../components/boat/BoatCatalogHero/BoatCatalogHero';
+import NoBoatsHeading from '../../components/ui/NoBoatsHeading/NoBoatsHeading';
 
 import Search from '../../components/ui/Search/Search';
 import Spinner from '../../components/ui/Spinner/Spinner';
+import Pagination from '../../components/ui/Pagination/Pagination';
 
+import queryUtil from '../../utils/queryUtil';
 
-import styles from '../../components/boat/BoatList/BoatList.module.css';
 
 const BoatsCatalog = () => {
-
+    const { search } = useLocation();
     const dispatch = useDispatch();
     const boats = useSelector(state => state.allBoats.boats);
+    const boatsCount = useSelector(state => state.allBoats.boatsCount);
     const { isLoading, requester } = useFetch();
+
+    const isSearch = ['type', 'fuel', 'price'].some(x => search.includes(x));
+
+    const totalPages = Math.ceil(boatsCount / 9);
 
     const heading = 'Boats';
     const subHeading = <FontAwesomeIcon icon={faDharmachakra} />;
 
+    const {page} = queryUtil.queryExtract(search);
 
     const responseData = useCallback((data) => {
         dispatch(boatStoreActions.addBoats(data));
     }, [dispatch]);
 
     useEffect(() => {
-        requester(boatService.getAll(), responseData);
-    }, [requester, responseData, dispatch]);
+        requester(boatService.getAll(search), responseData);
+    }, [requester, responseData, dispatch, search]);
 
     return (
         <>
             <BoatCatalogHero />
             <Search />
-            <section className={styles.boats}>
+            <section className='section-container'>
                 {isLoading && <Spinner size={'large'} />}
                 {!isLoading
                     && boats.length > 0
@@ -49,7 +58,16 @@ const BoatsCatalog = () => {
                         subHeading={subHeading}
                     />
                 }
+                {!isLoading 
+                && boats.length < 1
+                && <NoBoatsHeading isSearch={isSearch}/>
+                }
             </section>
+            {!isLoading && boatsCount > 9 && <Pagination
+                totalPages={totalPages}
+                page={page}
+                isLoading={isLoading}
+            />}
         </>
     )
 };
