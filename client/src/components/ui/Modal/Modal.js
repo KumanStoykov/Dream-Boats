a56809fa5735889357e7f0bdca9fa70ea3a78610
Kuman Stoykov <kumanstoykov@gmail.com>
2022-08-05 23:37:36 +0200

    Added close handler

--- client/src/components/ui/Modal/Modal.js
+++ client/src/components/ui/Modal/Modal.js
@@ -1,33 +1,25 @@
-import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { modalStoreActions } from '../../../store/modalStore';
 
 import styles from './Modal.module.css';
 
-
-export const Overlay = ({ onClose, children }) => {
-    return <div className={styles.modal} onClick={onClose} >{children}</div>;
+export const Overlay = ({ closeHandler, children }) => {
+    return <div className={styles.modal} onClick={closeHandler} >{children}</div>;
 }
 
-
-const Modal = ({ onClose, children }) => {
+const Modal = ({ children }) => {
     const dispatch = useDispatch();
     const portal = document.getElementById('portal');
     const modalState = useSelector(state => state.modal);
-   
 
-    useEffect(() => {
-        setTimeout(() => {
-            dispatch(modalStoreActions.close());
-        }, 5000);
-    }, [dispatch]);
-
+    const closeHandler = () => {
+        dispatch(modalState.close())
+    };
 
     return (
         <>
-            {modalState.isOpen && createPortal(<Overlay onClose={onClose}>{children}</Overlay>, portal)}
+            {modalState.isOpen && createPortal(<Overlay onClose={closeHandler}>{children}</Overlay>, portal)}
         </>
     );
 };
