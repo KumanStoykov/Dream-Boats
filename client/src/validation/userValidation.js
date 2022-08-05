0b059990e3c399dc81b998a8e297e3a37e01310c
Kuman Stoykov <kumanstoykov@gmail.com>
2022-08-05 23:48:37 +0200

    Added is empty validation

--- client/src/validation/userValidation.js
+++ client/src/validation/userValidation.js
@@ -10,12 +10,15 @@ const nameIsLength = (value) => validator.isLength(value, { min: 2 });
 
 const isEqual = (pass, rePass) => validator.equals(pass, rePass);
 
+const isEmpty = (value) => !(validator.isEmpty(value));
+
 const userValidation = {
     emailIsValid,
     isPhone,
     passwordIsLength,
     nameIsLength,
-    isEqual
+    isEqual,
+    isEmpty
 };
 
 export default userValidation;
