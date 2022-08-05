45174a0321f9311f1860ebb42fc2a90af6b7b005
Kuman Stoykov <kumanstoykov@gmail.com>
2022-08-05 23:39:29 +0200

    Update Search with useSearchParam

--- client/src/components/ui/Search/Search.js
+++ client/src/components/ui/Search/Search.js
@@ -1,60 +1,108 @@
-import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useSearchParams, useLocation, useNavigate } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAnchor } from '@fortawesome/free-solid-svg-icons';
+
+
 
 import styles from './Search.module.css';
 
 const Search = () => {
     const navigate = useNavigate();
+    const { search, pathname } = useLocation();
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [inputState, setInputState] = useState({ type: '', fuel: '', price: 0 });
+
+    useEffect(() => {
+        searchParams.set('type', inputState.type);
+        searchParams.set('fuel', inputState.fuel);
+        searchParams.set('price', inputState.price);
+    }, [inputState, search]);
+
+    const changeHandler = (type, e) => {
+        setInputState(state => ({
+            ...state,
+            [type]: e.target.value
+
+        }));
+    };
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        if (!pathname.includes('boats-for-sale')) {
+            const type = searchParams.get('type');
+            const fuel = searchParams.get('fuel');
+            const price = searchParams.get('price');
+
+            navigate(`/boats-for-sale?page=1&sort=desc&type=${type}&fuel=${fuel}&price=${price}`)
+        } else {
+            setSearchParams(searchParams);
+        }
+    }
 
     return (
         <section className={styles.search}>
             <div className={'container'}>
-                <form className={styles['book-form']}>
+                <form onSubmit={submitHandler} className={styles['book-form']}>
                     <div className={styles['input-group']}>
                         <label htmlFor="type" className={styles['input-label']}>Category</label>
-                        <select name="type" id="type" className={styles.options}>
-                            <option value={'any'}>Any</option>
+                        <select
+                            name="type"
+                            id="type"
+                            className={styles.options}
+                            value={inputState.type}
+                            onChange={changeHandler.bind(null, 'type')}
+                        >
+                            <option value={''}>Any</option>
                             <option value={'yacht'}>Yacht</option>
                             <option value={'motorboat'}>Motorboat</option>
                             <option value={'sailboat'}>Sailboat</option>
                         </select>
-                    </div>                   
+                    </div>
                     <div className={styles['input-group']}>
-                        <label htmlFor="priceFrom" className={styles['input-label']}>Price from</label>
-                        <select name="priceFrom" id="priceFrom" className={styles.options}>
-                            <option value={0}>0</option>
-                            <option value={1}>10,000$</option>
-                            <option value={2}>30,000$</option>
-                            <option value={4}>50,000$</option>
-                            <option value={5}>70,000$</option>
-                            <option value={6}>100,000$</option>
-                            <option value={7}>200,000$</option>
-                            <option value={8}>500,000$</option>
+                        <label htmlFor="fuel" className={styles['input-label']}>Fuel</label>
+                        <select
+                            name="fuel"
+                            id="fuel"
+                            className={styles.options}
+                            value={inputState.fuel}
+                            onChange={changeHandler.bind(null, 'fuel')}
+                        >
+                            <option value={''}>Any</option>
+                            <option value={'petrol'}>Petrol</option>
+                            <option value={'diesel'}>Diesel</option>
                         </select>
                     </div>
                     <div className={styles['input-group']}>
-                        <label htmlFor="priceTo" className={styles['input-label']}>Price to</label>
-                        <select name="priceTo" id="priceTo" className={styles.options}>
+                        <label htmlFor="priceFrom" className={styles['input-label']}>Price</label>
+                        <select
+                            name="price"
+                            id="price"
+                            className={styles.options}
+                            value={inputState.price}
+                            onChange={changeHandler.bind(null, 'price')}
+                        >
                             <option value={0}>0</option>
-                            <option value={1}>10,000$</option>
-                            <option value={2}>30,000$</option>
-                            <option value={4}>50,000$</option>
-                            <option value={5}>70,000$</option>
-                            <option value={6}>100,000$</option>
-                            <option value={7}>200,000$</option>
-                            <option value={8}>500,000$</option>
+                            <option value={10000}>10,000$</option>
+                            <option value={30000}>30,000$</option>
+                            <option value={50000}>50,000$</option>
+                            <option value={70000}>70,000$</option>
+                            <option value={100000}>100,000$</option>
+                            <option value={200000}>200,000$</option>
+                            <option value={500000}>500,000$</option>
                         </select>
                     </div>
 
-                    <button type="submit" className={'btn-blue'} >Search
+                    <button
+                        className={'btn-blue'}
+                    >Search
                         <span className={styles.anchor}><FontAwesomeIcon icon={faAnchor} /></span>
                     </button>
                 </form>
             </div>
         </section>
-
     );
 };
 
