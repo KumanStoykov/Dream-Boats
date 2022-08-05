fa2b182a402a204d7584bdcfafee45ac0ae46c56
Kuman Stoykov <kumanstoykov@gmail.com>
2022-08-05 23:33:49 +0200

    Added modals

--- client/src/components/layout/Layout/Layout.js
+++ client/src/components/layout/Layout/Layout.js
@@ -5,6 +5,8 @@ import Header from '../Header/Header';
 
 import ScrollToTopButton from '../../ui/ScrollToTopButton/ScrollToTopButton';
 import ErrorModal from '../../ui/ErrorModal/ErrorModal';
+import DeleteModal from '../../ui/DeleteModal/DeleteModal';
+import EmailModal from '../../ui/EmailModal/EmailModal';
 
 
 const Layout = (props) => {
@@ -12,7 +14,10 @@ const Layout = (props) => {
 
     return (
         <>
-            {modal.isOpen && <ErrorModal />}
+            {modal.isOpen && modal.type === 'error' && <ErrorModal />}
+            {modal.isOpen && modal.type === 'delete' && <DeleteModal />}
+            {modal.isOpen && modal.type === 'email' && <EmailModal />}
+            
             <ScrollToTopButton />
             <Header />
             <main>{props.children}</main>
