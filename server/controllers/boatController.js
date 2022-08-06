22d24a0a1bdaa43eff1e72539988e7f1f65dce4c
Kuman Stoykov <kumanstoykov@gmail.com>
2022-08-06 17:02:51 +0200

    Added sort parameter

--- server/controllers/boatController.js
+++ server/controllers/boatController.js
@@ -41,8 +41,9 @@ router.get('/', async (req, res) => {
 
 router.get('/getLastThree', async (req, res) => {
     try {
+        const sort = req.query.sort;
 
-        const boats = await boatService.getLastThree();
+        const boats = await boatService.getLastThree(sort);
         res.status(200).send({ boats });
     } catch (error) {
         res.status(400).send({ message: error.message });
