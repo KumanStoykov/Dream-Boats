128a4ae304d480023b30a2ae65b702f53f297146
Kuman Stoykov <kumanstoykov@gmail.com>
2022-08-06 18:47:05 +0200

    Changed sript

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 require('dotenv').config();
 
 const cloudinary = require('cloudinary').v2;
@@ -15,19 +16,16 @@ expressInit(app);
 
 cloudinary.config(config.CLOUDINARY);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 databaseInit(config.DB_CONNECTION_STRING)
     .then(() => {
-        router.get('*', (req, res) => {
-
-            const formats = ['.js', '.css', '.ico', '.jpg', '.png']
-
-            if (formats.filter(ext => req.url.indexOf(ext) > 0).length > 0) {
-                res.sendFile(path.resolve(`public/${req.url}`));
-            } else {
-                res.sendFile(path.join(__dirname, "public/index.html"));
-            }
-        });
         app.listen(config.PORT, () => console.log(`App listen in port: ${config.PORT}...`));
     })
     .catch(err => console.log('Database init is failed!', err));
