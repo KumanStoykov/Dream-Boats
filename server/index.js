4d92946ac7a17acbba2e9ff5afd094ee05654395
Kuman Stoykov <kumanstoykov@gmail.com>
2022-08-06 22:46:23 +0200

    Added path

--- server/index.js
+++ server/index.js
@@ -18,17 +18,17 @@ cloudinary.config(config.CLOUDINARY);
 
 const formats = ['.js', '.css', '.ico', '.jpg', '.png'];
 
-app.get('*', (req, res) => {
-
-    if (formats.filter(ext => req.url.indexOf(ext) > 0).length > 0) {
-        res.sendFile(path.resolve(`public/${req.url}`));
-    } else {
-        res.sendFile(path.join(__dirname, 'public/index.html'));
-    }
-});
 
 databaseInit(config.DB_CONNECTION_STRING)
     .then(() => {
+        app.get('*', (req, res) => {
+
+            if (formats.filter(ext => req.url.indexOf(ext) > 0).length > 0) {
+                res.sendFile(path.resolve(`public/${req.url}`));
+            } else {
+                res.sendFile(path.join(__dirname, 'public/index.html'));
+            }
+        });
         app.listen(config.PORT, () => console.log(`App listen in port: ${config.PORT}...`));
     })
     .catch(err => console.log('Database init is failed!', err));
