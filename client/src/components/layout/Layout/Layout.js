77c222366e5a209e07a27c069e16b19d0e3b9e5e
Kuman Stoykov <kumanstoykov@gmail.com>
2022-08-07 16:14:55 +0200

    make it better

--- client/src/components/layout/Layout/Layout.js
+++ client/src/components/layout/Layout/Layout.js
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 
-
+import ScrollToTopButton from '../../ui/ScrollToTopButton/ScrollToTopButton';
 import ErrorModal from '../../ui/ErrorModal/ErrorModal';
 import DeleteModal from '../../ui/DeleteModal/DeleteModal';
 import EmailModal from '../../ui/EmailModal/EmailModal';
@@ -18,6 +18,7 @@ const Layout = (props) => {
             {modal.isOpen && modal.type === 'delete' && <DeleteModal />}
             {modal.isOpen && modal.type === 'email' && <EmailModal />}
             
+            <ScrollToTopButton />
             <Header />
             <main>{props.children}</main>
             <Footer />
