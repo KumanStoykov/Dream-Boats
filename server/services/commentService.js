30aeabec682e3ae84b8072be031b8a702ccf98ab
Kuman Stoykov <kumanstoykov@gmail.com>
2022-08-11 18:12:11 +0200

    Added get, edit, and create comment services

--- server/services/commentService.js
+++ server/services/commentService.js
@@ -2,4 +2,16 @@ const Comment = require('../models/Comment');
 
 exports.getAll = () => Comment.find({}).sort({ createdAt: 'desc' });
 
-exports.create = (data) => Comment.create(data);
+exports.getOne = (commentId) => Comment.findById(commentId);
+
+exports.create = (commentData) => Comment.create(commentData);
+
+exports.edit = async (commentId, data) => {
+    const comment = await Comment.findById(commentId);
+
+    Object.assign(comment, data);
+
+    return comment.save();
+};
+
+exports.commentDelete = (commentId) => Comment.findByIdAndDelete(commentId);
