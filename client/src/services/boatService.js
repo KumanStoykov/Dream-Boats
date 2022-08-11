82f9e9af275f11a01ff4d6565eb4380fff891b1c
Kuman Stoykov <kumanstoykov@gmail.com>
2022-08-11 18:02:25 +0200

    Change base url

--- client/src/services/boatService.js
+++ client/src/services/boatService.js
@@ -1,7 +1,6 @@
-const BASE_URL = 'https://dream-boats.herokuapp.com';
+const BASE_URL = process.env.REACT_APP_SERVER_API_URL;
 
-const getAll = (search) => {    
-    console.log(BASE_URL)
+const getAll = (search) => {
     const options = {
         url: `${BASE_URL}/boat${search}`
     };
