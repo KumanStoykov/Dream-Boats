392c87971a2b21fcc4cc3e07ab715e0f036966b2
Kuman Stoykov <kumanstoykov@gmail.com>
2022-08-11 18:02:44 +0200

    Added edit and delete comment

--- client/src/services/commentService.js
+++ client/src/services/commentService.js
@@ -9,7 +9,7 @@ const getComments = () => {
 
 const createComment = (name, comment, rating, ownerId) => {
     const options = {
-        url: `${BASE_URL}/comment/create/${ownerId}`,
+        url: `${BASE_URL}/comment/${ownerId}`,
         method: 'POST',       
         headers: {
             'Content-Type': 'application/json'
@@ -24,10 +24,36 @@ const createComment = (name, comment, rating, ownerId) => {
 
     return options;
 };
+const editComment = (name, comment, rating, commentId) => {
+    const options = {
+        url: `${BASE_URL}/comment/${commentId}`,
+        method: 'PUT',       
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name,
+            comment,
+            rating            
+        })
+    };
+
+    return options;
+};
+const deleteComment = (commentId) => {
+    const options = {
+        url: `${BASE_URL}/comment/${commentId}`,
+        method: 'DELETE',       
+    };
+
+    return options;
+};
 
 const emailRequestOptions = {
     getComments,
-    createComment
+    createComment,
+    editComment,
+    deleteComment
 };
 
 export default emailRequestOptions;
