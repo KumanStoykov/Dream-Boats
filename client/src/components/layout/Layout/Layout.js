ccb3986881c5e16021ebc9912e7dc80ca2c19059
Kuman Stoykov <kumanstoykov@gmail.com>
2022-08-11 17:53:13 +0200

    Added more Notifications

--- client/src/components/layout/Layout/Layout.js
+++ client/src/components/layout/Layout/Layout.js
@@ -4,19 +4,22 @@ import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 
 import ScrollTop from '../../ui/ScrollTop/ScrollTop';
-import ErrorModal from '../../ui/ErrorModal/ErrorModal';
+import NotificationModal from '../../ui/NotificationModal/NotificationModal';
 import DeleteModal from '../../ui/DeleteModal/DeleteModal';
 import EmailModal from '../../ui/EmailModal/EmailModal';
+import CommentModal from '../../comment/CommentModal/CommentModal';
 
 
 const Layout = (props) => {
     const modal = useSelector(state => state.modal);
+    const isNotification = ['error', 'secssful'].some(x => x === modal.type);
 
     return (
         <>
-            {modal.isOpen && modal.type === 'error' && <ErrorModal />}
+            {modal.isOpen && isNotification && <NotificationModal />}
             {modal.isOpen && modal.type === 'delete' && <DeleteModal />}
             {modal.isOpen && modal.type === 'email' && <EmailModal />}
+            {modal.isOpen && modal.type === 'comment' && <CommentModal />}
             
             <ScrollTop />
             <Header />
