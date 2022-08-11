38717aeadddcd0c69d4535021f3bdf40c016a6ef
Kuman Stoykov <kumanstoykov@gmail.com>
2022-08-11 17:46:11 +0200

    Added guards and auto load user

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
+import { useEffect, useCallback } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 import './App.css';
 
@@ -13,27 +15,52 @@ import BoatsCatalog from './pages/BoatsCatalog/BoatsCatalog';
 import DetailsBoat from './pages/DetailsBoat/DetailsBoat';
 import Profile from './pages/Profile/Profile';
 
+import useFetch from './hooks/useFetch';
+import userService from './services/userService';
+import { authStoreActions } from './store/authStore';
+
+import UserProfileGuard from './components/common/UserProfileGuard';
 import AuthGuard from './components/common/AuthGuard';
 
+import Spinner from './components/ui/Spinner/Spinner';
+
 function App() {
-
-
 	useScrollToTop();
+
+	const dispatch = useDispatch();
+	const { isLoading, requester } = useFetch();
+
+	const responseData = useCallback((data) => {
+		dispatch(authStoreActions.login(data));
+		dispatch(authStoreActions.loadUser());
+	}, [dispatch]);
+
+
+	useEffect(() => {
+		requester(userService.checkUser(), responseData);
+	}, [dispatch, requester]);
 
 	return (
 		<Layout>
+			{isLoading && <Spinner size={'large'} />}
 			<Routes>
 				<Route path='/' element={<Home />} />
-				<Route path='/watch-list' element={<WatchList />}/>
-				<Route path='/auth'>
-					<Route path='login' index element={<Login />} />
-					<Route path='register' element={<Register />} />
-				</ Route>
+				<Route path='/watch-list' element={<WatchList />} />
+				
+				<Route element={<AuthGuard />}>
+					<Route path='/auth'>
+						<Route path='login' index element={<Login />} />
+						<Route path='register' element={<Register />} />
+					</ Route>
+				</Route>
+
 				<Route path='/boats-for-sale' element={<BoatsCatalog />} />
 				<Route path='/boat/details/:boatId' element={<DetailsBoat />} />
-				<Route element={<AuthGuard />}>
-					<Route path='/profile/*' element={<Profile />} />	
-				</Route>				
+
+				<Route element={<UserProfileGuard />}>
+					<Route path='/profile/*' element={<Profile />} />
+				</Route>
+
 				<Route path='*' element={<Navigate to={'/'} />} />
 			</Routes>
 		</Layout>
