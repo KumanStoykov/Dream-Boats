378f9089ab4cc81faca7153b650261135502d2f0
Kuman Stoykov <kumanstoykov@gmail.com>
2022-08-11 18:05:48 +0200

    Added phone regex pattern

--- client/src/validation/userValidation.js
+++ client/src/validation/userValidation.js
@@ -2,11 +2,13 @@ import validator from 'validator';
 
 const emailIsValid = (value) => validator.isEmail(value);
 
-const isPhone = (value) => validator.isMobilePhone(value);
+const isPhone = (value) => validator.matches(value, /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im);
 
 const passwordIsLength = (value) => validator.isLength(value, { min: 5 });
 
 const nameIsLength = (value) => validator.isLength(value, { min: 2 });
+
+const textIsLength = (value) => validator.isLength(value, { min: 10 });
 
 const isEqual = (pass, rePass) => validator.equals(pass, rePass);
 
@@ -17,6 +19,7 @@ const userValidation = {
     isPhone,
     passwordIsLength,
     nameIsLength,
+    textIsLength,
     isEqual,
     isEmpty
 };
