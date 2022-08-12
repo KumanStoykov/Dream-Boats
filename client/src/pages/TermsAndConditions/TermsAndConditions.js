7cf4fbdd51386f3493607b11df967a74b2b56567
Kuman Stoykov <kumanstoykov@gmail.com>
2022-08-12 22:39:23 +0200

    Create Terms and conditions

---
+++ client/src/pages/TermsAndConditions/TermsAndConditions.js
@@ -0,0 +1,55 @@
+import styles from './TermsAndConditions.module.css';
+
+const TermsAndConditions = () => {
+
+
+    return (
+        <section className={styles.TermsAndConditions}>
+
+            <article className={styles['heading-wrapper']}>
+                <h1 className={styles.heading}>Terms And Conditions</h1>
+            </article>
+            <article className={styles['p-wrapper']}>
+                <p className={styles['p-text']}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Perferendis ab ipsa et voluptatibus dignissimos, sapiente tempora nam velit
+                    explicabo eius odio architecto qui ducimus cumque inventore ut rerum voluptas,
+                    sed voluptatem. Necessitatibus rem similique, adipisci tempora id quas provident
+                    aut iusto? Quaerat cumque magni hic, officia voluptatum odit nostrum magnam!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+                <p className={styles['p-text']}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Perferendis ab ipsa et voluptatibus dignissimos, sapiente tempora nam velit
+                    explicabo eius odio architecto qui ducimus cumque inventore ut rerum voluptas,
+                    sed voluptatem. Necessitatibus rem similique, adipisci tempora id quas provident
+                    aut iusto? Quaerat cumque magni hic, officia voluptatum odit nostrum magnam!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+                <p className={styles['p-text']}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Perferendis ab ipsa et voluptatibus dignissimos, sapiente tempora nam velit
+                    explicabo eius odio architecto qui ducimus cumque inventore ut rerum voluptas,
+                    sed voluptatem. Necessitatibus rem similique, adipisci tempora id quas provident
+                    aut iusto? Quaerat cumque magni hic, officia voluptatum odit nostrum magnam!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+                <p className={styles['p-text']}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Perferendis ab ipsa et voluptatibus dignissimos, sapiente tempora nam velit
+                    explicabo eius odio architecto qui ducimus cumque inventore ut rerum voluptas,
+                    sed voluptatem. Necessitatibus rem similique, adipisci tempora id quas provident
+                    aut iusto? Quaerat cumque magni hic, officia voluptatum odit nostrum magnam!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+                <p className={styles['p-text']}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Perferendis ab ipsa et voluptatibus dignissimos, sapiente tempora nam velit
+                    explicabo eius odio architecto qui ducimus cumque inventore ut rerum voluptas,
+                    sed voluptatem. Necessitatibus rem similique, adipisci tempora id quas provident
+                    aut iusto? Quaerat cumque magni hic, officia voluptatum odit nostrum magnam!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </article>
+
+
+        </section>
+    );
+};
+
+export default TermsAndConditions;
