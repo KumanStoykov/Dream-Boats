e8e5ada3d159e9a36b35089116950b806211bd72
Kuman Stoykov <kumanstoykov@gmail.com>
2022-08-13 12:33:32 +0200

    Fix url to catalogue

--- client/src/components/home/Hero/Hero.js
+++ client/src/components/home/Hero/Hero.js
@@ -5,7 +5,7 @@ import { faEllipsisH } from '@fortawesome/free-solid-svg-icons';
 import styles from './Hero.module.css';
 
 const Hero = () => {
-    
+
 
     return (
         <div className={styles.hero}>
@@ -14,8 +14,8 @@ const Hero = () => {
                     <h1 className={styles.title}>Dream Boats</h1>
                     <h2 className={styles.subtitle}>Search your dream boat</h2>
                 </div>
-                <Link to="/boats-for-sale" className={'btn-gradient'}>Search now                
-                <span className={'dots'}><FontAwesomeIcon icon={faEllipsisH}/></span>                                   
+                <Link to='/boats-for-sale?page=1&sort=desc' className={'btn-gradient'}>Search now
+                    <span className={'dots'}><FontAwesomeIcon icon={faEllipsisH} /></span>
                 </Link>
             </div>
         </div>
