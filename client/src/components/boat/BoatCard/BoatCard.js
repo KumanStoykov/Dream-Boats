59a9b58c8409c4ccfdaa26d1d57945d87ab3c17f
Kuman Stoykov <kumanstoykov@gmail.com>
2022-08-19 22:50:05 +0200

    Added image alt

--- client/src/components/boat/BoatCard/BoatCard.js
+++ client/src/components/boat/BoatCard/BoatCard.js
@@ -11,7 +11,7 @@ const BoatCard = ({
 }) => {
     return (
         <li className={`grid-item ${styles['featured-boats']}`}>
-            <img src={boat.image[0]?.url} alt="image.jpg" className={`${styles['boat-image']} ${styles[fancyBoarder]}`} />
+            <img src={boat.image[0]?.url} alt="boat.jpg" className={`${styles['boat-image']} ${styles[fancyBoarder]}`} />
             <h5 className={styles['boat-make']}>{boat.make}</h5>
             <p className={styles['boat-model']}>{boat.model}</p>
             <span className={styles['boat-price']}>{boat.price.toLocaleString()}$</span>
